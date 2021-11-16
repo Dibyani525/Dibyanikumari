e0918171b18754b05cf09e6fff40ef3809ba0f0a
Dibyani525 <94382129+Dibyani525@users.noreply.github.com>
2021-11-16 10:40:29 +0545

    Create Ifelse.java

---
+++ Ifelse.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ifElse {
+	public static void main (String args[]) { 
+		int a = 300;
+		int b = 100;
+		
+		if(a > b) {
+			System.out.print(a);
+		}else {
+			System.out.print(b);
+		}
+	}
+}
