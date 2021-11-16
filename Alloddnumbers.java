cde2ddd96d8dc9b8665c47cf4bb63131691b554b
Dibyani525 <94382129+Dibyani525@users.noreply.github.com>
2021-11-16 10:34:07 +0545

    Create Alloddnumbers.java

---
+++ Alloddnumbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class allOddNumbers {
+	
+	public static void main (String args[]) {
+		
+		for (int i = 1; i< 100; i=i+1) {
+			
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+			
+		}
+		
+	}
+
+}
