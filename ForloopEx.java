d40269a2baa88aa04306c48101daa25b220bd875
Dibyani525 <94382129+Dibyani525@users.noreply.github.com>
2021-11-16 10:35:34 +0545

    Create ForloopEx.java

---
+++ ForloopEx.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class forLoopEx {
+	
+	public static void main (String args[]) {
+		
+		for (int num = 500; num >= 1; num = num-1) {
+			System.out.println(num); 
+		}
+		
+		for (int my_num = 2; my_num <= 100; my_num = my_num+2) {
+			System.out.println(my_num); 
+		}
+	}
+}
