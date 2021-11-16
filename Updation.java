2698d1a8951e31013ff2bf54cc34bd0810cc31d3
Dibyani525 <94382129+Dibyani525@users.noreply.github.com>
2021-11-16 10:44:07 +0545

    Create Updation.java

---
+++ Updation.java
@@ -0,0 +1,22 @@
+public class updation {
+	public static void main (String args[]) {
+		
+		int num = 100;
+		
+		num = num + 10;
+		System.out.println(num);
+		
+		
+		num = num - 30;
+		System.out.println(num); 
+		
+		
+		num = num * 5;
+		System.out.println(num);
+		
+		num = num/10;
+		System.out.println(num); 
+		
+		
+	}
+}
