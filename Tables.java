d8bf35c4b7856b6d4826c392b96855932e3c3756
Dibyani525 <94382129+Dibyani525@users.noreply.github.com>
2021-11-16 10:42:15 +0545

    Create Tables.java

---
+++ Tables.java
@@ -0,0 +1,13 @@
+public class tables{
+	
+	public static void main (String args[]) {
+		
+		Scanner sc = new Scanner(System.in);  
+		int a=sc.nextInt();  
+		for (int num = 1; num <= 10; num = num+1) {
+			System.out.println(num*a);
+		}
+		
+	}
+
+}
