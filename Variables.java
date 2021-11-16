82693700be3715ed4fe046ee6913ef9ae5253aac
Dibyani525 <94382129+Dibyani525@users.noreply.github.com>
2021-11-16 10:44:52 +0545

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class variable{  
+    public static void main(String args[]){  
+     
+	 byte a=20;
+	 short s=1000;
+	 int i=100000;
+	 long l=100000L;
+	 float f=219.2f;
+	 double d=24.35;
+	 char c='A';
+	 boolean b=false;
+	 
+	 System.out.println(a); 
+	 System.out.println(s); 
+	 System.out.println(i); 
+	 System.out.println(l); 
+	 System.out.println(f); 
+	 System.out.println(d); 
+	 System.out.println(c); 
+	 System.out.println(b); 
+	 
+    }  
+}
