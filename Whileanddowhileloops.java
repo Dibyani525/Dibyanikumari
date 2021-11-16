0937735f9e09a26e1bbf2f4a42572d93c5ab3faa
Dibyani525 <94382129+Dibyani525@users.noreply.github.com>
2021-11-16 10:45:48 +0545

    Create Whileanddowhileloops.java

---
+++ Whileanddowhileloops.java
@@ -0,0 +1,18 @@
+public class whileAndDoWhileLoops {
+
+	public static void main (String args[]) {
+		
+		int n= 200;
+		while (n<=100) {
+			System.out.println(n);
+			n = n+1;
+		}
+		
+		int num2 = 500;
+		do {
+			System.out.println(num2);
+			num2 = num2+1;
+		} while (num2<=100);
+	
+	}
+}
