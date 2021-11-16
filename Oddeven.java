1d5cae2548544bd984c5e871af1a572cd7795a3a
Dibyani525 <94382129+Dibyani525@users.noreply.github.com>
2021-11-16 10:41:17 +0545

    Create Oddeven.java

---
+++ Oddeven.java
@@ -0,0 +1,13 @@
+public class oddEven {
+
+	public static void main (String args[]) {
+		
+		int i = 311;
+		
+		if(i % 2 ==0) {
+			System.out.print("Even");
+		}else {
+			System.out.print("Odd");
+		}
+	}
+}
